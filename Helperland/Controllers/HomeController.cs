374746473c7091bf9f753d1b554f273a188b585b
Jhanviparmar <66872551+Jhanviparmar@users.noreply.github.com>
2022-01-30 15:47:14 +0530

    Add files via upload

---
+++ Helperland/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using Helperland.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Helperland.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Helperland.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        HelperlanddContext db = new HelperlanddContext();
+        ////private readonly HelperlanddContext _DbContext;
+
+        ////public HomeController(HelperlanddContext DbContext)
+        ////{
+        ////    _DbContext = DbContext;
+        ////}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult faqs()
+        {
+            return View();
+        }
+
+        public IActionResult Price()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        { 
+            return View();
+        }
+        [HttpPost]
+        
+        public IActionResult Contact(ContactU contactu)
+        {
+            ContactU contactUs = new ContactU();
+            contactUs.Name = contactu.Name + contactu.Name;
+            contactUs.Email = contactu.Email;
+            contactUs.PhoneNumber = contactu.PhoneNumber;
+            contactUs.Message = contactu.Message;
+            contactUs.Subject = contactu.Subject;
+            contactUs.CreatedOn = DateTime.Now;          
+            db.ContactUs.Add(contactu);
+            db.SaveChanges();
+            return View("Contact");
+        }
+
+public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
